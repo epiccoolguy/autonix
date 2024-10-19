a523f42b45265eda0d2cfa822690cfdf2ac97b48
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-10-19 10:09:34 +0200

    download and install nix

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ softwareupdate --install --no-scan "$PRODUCT"
 
 # remove temporary file
 rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+
+# download and install nix
+curl -fsSL https://nixos.org/nix/install | sh -s -- --yes
+
+# launch a new shell to load system-wide profile changes from nix
+exec zsh
