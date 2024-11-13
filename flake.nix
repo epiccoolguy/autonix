6fefdbb99cfe51e6fbb6973506e199131f733887
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-11-14 00:14:53 +0100

    add vscode placeholder

--- flake.nix
+++ flake.nix
@@ -162,6 +162,15 @@
               credential.useHttpPath = true;
             };
           };
+
+          programs.vscode = {
+            enable = true;
+            extensions = with pkgs.vscode-extensions; [
+              dracula-theme.theme-dracula
+              vscodevim.vim
+              yzhang.markdown-all-in-one
+            ];
+          };
         };
     in
     {
