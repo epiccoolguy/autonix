0c750e6cadac74ae6bbf237a0d8e3f6ffac426d6
Miguel Lo-A-Foe <miguel@loafoe.dev>
2025-07-17 23:12:38 +0200

    add mac-app-util

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
   outputs =
@@ -15,6 +16,7 @@
       nix-darwin,
       nixpkgs,
       home-manager,
+      mac-app-util,
     }:
     {
       # Build darwin flake using:
@@ -23,11 +25,15 @@
         "Miguels-MacBook-Air" = nix-darwin.lib.darwinSystem {
           modules = [
             ./darwin/miguel.nix
+            mac-app-util.darwinModules.default
             home-manager.darwinModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.miguel = import ./home/miguel.nix;
+              home-manager.sharedModules = [
+                mac-app-util.homeManagerModules.default
+              ];
             }
           ];
           specialArgs = { inherit inputs; };
