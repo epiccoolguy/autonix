0450650b19a0d94a31b00942229eeeaddf7aaab3
Miguel Lo-A-Foe <miguel@loafoe.dev>
2026-02-02 16:12:18 +0100

    add only specific cert env var

--- install.sh
+++ install.sh
@@ -57,6 +57,6 @@ sudo mv /etc/zshrc /etc/zshrc.before-nix-darwin
 sudo mv /etc/nix/nix.conf /etc/nix/nix.conf.before-nix-darwin
 
 # install nix-darwin using flakes, rebuild the system and switch to the new generation
-sudo -HE nix run --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin -- switch --flake "/etc/nix-darwin"
+sudo -H NIX_SSL_CERT_FILE=$CERT nix run --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin -- switch --flake "/etc/nix-darwin"
 
 echo 'Done setting up the system. Restart the shell for the "switch" command to become available.'
